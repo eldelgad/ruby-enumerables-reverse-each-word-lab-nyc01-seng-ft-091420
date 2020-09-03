8a5aa30413dd43021d8667a002023e21585182eb
Elias Delgado <evdelgado99@gmail.com>
2020-09-03 08:15:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   
   string = string.split
   
-  string.each do |element| element.reverse_each_word
+  string.each do |element| element.reverse
     return string
   end
   
